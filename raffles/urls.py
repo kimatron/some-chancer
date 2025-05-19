d8e68711b4b155c126ed1faccbab327c037145ce
kimatron <kimmykim@gmail.com>
2025-05-20 00:31:48 +0100

    Fix logout path not working

--- raffles/urls.py
+++ raffles/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     
     # Authentication
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html', next_page='raffles:home'), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     
     # User dashboard

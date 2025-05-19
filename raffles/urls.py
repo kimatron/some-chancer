c2b77bbb109ad3d0281e567a5afc26766895f90a
kimatron <kimmykim@gmail.com>
2025-05-19 15:37:12 +0100

    Set up URL routing for main pages

---
+++ raffles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'raffles'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('raffles/', views.raffle_list, name='raffle_list'),
+    path('raffle/<int:raffle_id>/', views.raffle_detail, name='raffle_detail'),
+]
